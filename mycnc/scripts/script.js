0f66d2072b8b56a61716fc2123ea0e16321fb847
peter <peter@DT_PETER>
2023-03-18 08:33:59 +0200

    TheBuild Modal component for all images

--- mycnc/scripts/script.js
+++ mycnc/scripts/script.js
@@ -8,4 +8,26 @@ function closeNav() {
   document.getElementById("main").style.marginLeft = "0";
 }
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Open the modal
+function openModal(img) {
+  // var img = document.getElementById("myImg");
+  var img = img
+  var modalImg = document.getElementById("img01");
+  var captionText = document.getElementById("caption");
+  modal.style.display = "block";
+  modalImg.src = img.src;
+  captionText.innerHTML = img.alt;
+}
+
+// Close the modal
+var closeBtn = document.getElementsByClassName("close")[0];
+closeBtn.onclick = function() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+}
+
+
 
